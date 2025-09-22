97f859bf00d1c537a8412134e8d22db15e529a44
buyath <buyaththayub@gmail.com>
2025-09-22 16:00:24 +0000

    feat: add hamburger button to navbar for mobile navigation

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const nav_items = document.querySelector("#nav-items");
+const nav_items = document.querySelector("#nav");
 const nav_logo = document.querySelector(".nav-logo-container");
 const body = document.getElementById("body");
 nav_logo.addEventListener("click",() => {
     nav_items.classList.toggle("nav-show");
 })
 
-if (window.innerWidth > 500) {
-    nav_items.classList.remove("nav-show");
-}
