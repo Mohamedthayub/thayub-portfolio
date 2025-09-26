46cecde79bcec991ef36e7940245b843538e9505
buyath <buyaththayub@gmail.com>
2025-09-26 23:52:58 +0000

    feat : added mobile responsive to about me page

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const nav_items = document.querySelector("#nav");
+const nav_items = document.querySelector("#nav-items");
 const nav_logo = document.querySelector(".nav-logo-container");
-const body = document.getElementById("body");
 nav_logo.addEventListener("click",() => {
     nav_items.classList.toggle("nav-show");
 })
